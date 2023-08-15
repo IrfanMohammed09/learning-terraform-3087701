98150ad98678f97ed79dbbc2c8200fb94b6829b7
IrfanMohammed09 <101171725+IrfanMohammed09@users.noreply.github.com>
2023-08-15 19:55:08 -0400

    sg with module

--- main.tf
+++ main.tf
@@ -17,49 +17,26 @@ data "aws_vpc" "default"{
 default=true
 }
 
-resource "aws_security_group" "firewall"{
-  name="Firewall"
-  description="Allow Http and Https inbound and all outbound"
-
-  vpc_id=data.aws_vpc.default.id
-}
-
-resource "aws_security_group_rule" "http_in" {
-  type        = "ingress"
-  from_port   = 80
-  to_port     = 80
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.firewall.id
-}
-
-
-resource "aws_security_group_rule" "https_in" {
-  type        = "ingress"
-  from_port   = 443
-  to_port     = 443
-  protocol    = "tcp"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.firewall.id
-}
-
-
-resource "aws_security_group_rule" "everything_out" {
-  type        = "egress"
-  from_port   = 0
-  to_port     = 0
-  protocol    = "-1"
-  cidr_blocks = ["0.0.0.0/0"]
-  security_group_id = aws_security_group.firewall.id
-}
-
 resource "aws_instance" "web" {
   ami           = data.aws_ami.app_ami.id
   instance_type = var.instance_type
 
-  vpc_security_group_ids=[aws_security_group.firewall.id]
+  vpc_security_group_ids=[module.mod_sg.security_group_id]
 
   tags = {
     Name = "HelloWorld"
   }
+
+  module "mod_sg" {
+  source  = "terraform-aws-modules/security-group/aws"
+  version = "4.13.0"
+
+  vpc_id  = data.aws_vpc.default.id
+  name    = "blog"
+  ingress_rules = ["https-443-tcp","http-80-tcp"]
+  ingress_cidr_blocks = ["0.0.0.0/0"]
+  egress_rules = ["all-all"]
+  egress_cidr_blocks = ["0.0.0.0/0"]
+}
+
 }
